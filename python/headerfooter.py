3551231041b605c005c4c757a3e6ff37233e370a
ricardo <ricardo.ribeiro@physics.org>
2020-06-18 19:28:09 +0100

    Create headfooter.py

---
+++ python/headerfooter.py
@@ -0,0 +1,23 @@
+
+def header(title,time):
+
+  print()
+  print('     Program '+title+' v.0.1 starts '+time)
+  print()
+  print('     This program is part of the open-source BERRY suite.')
+  print()
+
+
+def footer(time):
+
+  print()
+  print()
+  print('     '+time)
+
+  print()
+  print('      Program finished')
+
+
+
+
+
