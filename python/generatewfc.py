f0d86a3ded39906bdf870e0d9947fd98595f6e6d
LeanderReascos <49106116+LeanderReascos@users.noreply.github.com>
2021-01-30 16:40:31 +0000

    Generate Wave Functions

--- python/generatewfc.py
+++ python/generatewfc.py
@@ -27,7 +27,7 @@ import loaddata as d
 
 def extractconvert(nk):
   print(' Calculating wfc for k-point',nk)
-  for nb in range(1,d.nbnd+1):
+  for nb in range(d.nbnd):
     dft.wfck2r(nk,nb)
   return
 
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     nb = int(sys.argv[2])
     print(' Will run just for k-point',nk,'and band',nb)
   print()
-  npr=2
+#  npr=2
 #  print(mp.cpu_count())
   # Creates file with wfc of all bands at nk  ** DFT **
   if nk == -1 and nb == -1:                  # Will run for all k-points and bands
@@ -88,13 +88,12 @@ if __name__ == '__main__':
       with mp.Pool(processes=npr) as pool:
         pool.map(extractconvert, range(nks),int(npr))
     else:
-      for nk in range(0,nks):
+      for nk in range(nks):
         print(' Calculating wfc for k-point',nk)
-        for nb in range(1,d.nbnd+1):
-          dft.wfck2r(nk,nb)
+        dft.wfck2r(nk,0,d.nbnd-1)             #Calculate for each k all bands
   
   elif nk != -1 and nb == -1:                # Will run just for k-point nk
-    for nb in range(1,d.nbnd+1):
+    for nb in range(d.nbnd):
       print(' Calculating wfc for k-point',nk,'and band',nb)
       dft.wfck2r(nk,nb)
   
