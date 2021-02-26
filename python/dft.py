ac8d63f70bdc4cf646bfd62300093ea87c276d52
ricardoribeiro-2020 <ricardo@fisica.uminho.pt>
2021-02-26 19:33:48 +0000

    Corrected indentation

--- python/dft.py
+++ python/dft.py
@@ -2,173 +2,166 @@
 # These routines run the DFT calculations
 ###################################################################################
 
-import numpy as np
-# This is to make operations in the shell
 import os
 import sys
 import subprocess
 
+import numpy as np
+
 # SCF calculation
-def scf(mpi,directory,name_scf,outdir,pseudodir):
+def scf(mpi, directory, name_scf, outdir, pseudodir):
 
-  scffile = directory + name_scf
-  if scffile[-3:] != '.in':
-    verifica = os.popen("test -f " + scffile + ".out;echo $?").read()   #if 1\n does not exist if 0\n exists
-  else:
-    verifica = os.popen("test -f " + scffile[:-3] + ".out;echo $?").read()   #if 1\n does not exist if 0\n exists
-  
-  if verifica == '1\n':  # If output file does not exist, run the scf calculation
-    with open(scffile,'r') as templa:
-      original = templa.read()
-    templa.closed
-
-    # Changes file names to absolute paths: its safer
-    lines = original.split('\n')
-    subst = ''
-    for line in lines:
-      if line.find('outdir') >= 0:
-        subst = subst + line.find('outdir')*" "+"outdir = '" + outdir + "' ,\n"
-      elif line.find('pseudo_dir') >= 0:
-        subst = subst + line.find('pseudo_dir')*" "+"pseudo_dir = '" + pseudodir + "' ,\n"
-      else:
-        subst = subst + line.rstrip() + '\n'
-
-    # Saves file with absolute paths: overides original!
-    with open(scffile,'w') as output:
-      output.write(subst)
-    output.closed
-
-
-    print('     Running scf calculation')
-    if scffile[-3:] != '.in':
-      comando = mpi + "pw.x -i " + scffile + " > " + scffile + ".out"
+    scffile = directory + name_scf
+    if scffile[-3:] != '.in':   #if 1\n does not exist if 0\n exists
+        verifica = os.popen("test -f " + scffile + ".out;echo $?").read()
     else:
-      comando = mpi + "pw.x -i " + scffile + " > " + scffile[:-3] + ".out"
-    print('     Command: '+comando+'\n')
-    os.system(comando)
-  return
+        verifica = os.popen("test -f " + scffile[:-3] + ".out;echo $?").read()
+
+    if verifica == '1\n':  # If output file does not exist, run the scf calculation
+        with open(scffile, 'r') as templa:
+            original = templa.read()
+        templa.closed
+
+        # Changes file names to absolute paths: its safer
+        lines = original.split('\n')
+        subst = ''
+        for line in lines:
+            if line.find('outdir') >= 0:
+                subst = subst + line.find('outdir')*" " + "outdir = '" + outdir + "' ,\n"
+            elif line.find('pseudo_dir') >= 0:
+                subst = subst + line.find('pseudo_dir')*" " + "pseudo_dir = '" + pseudodir + "' ,\n"
+            else:
+                subst = subst + line.rstrip() + '\n'
+
+        # Saves file with absolute paths: overides original!
+        with open(scffile, 'w') as output:
+            output.write(subst)
+        output.closed
+
+
+        print('     Running scf calculation')
+        if scffile[-3:] != '.in':
+            comando = mpi + "pw.x -i " + scffile + " > " + scffile + ".out"
+        else:
+            comando = mpi + "pw.x -i " + scffile + " > " + scffile[:-3] + ".out"
+        print('     Command: ' + comando + '\n')
+        os.system(comando)
 #    sys.exit("Stop")
 
 
 
 # Creates template for nscf calculation
-def template(directory,name_scf):
+def template(directory, name_scf):
 
-  # opens the template file from which it will do nscf calculations
-  scffile = directory + name_scf 
-  with open(scffile,'r') as templa:
-    original = templa.read()
-  templa.closed
-  nscf1 = original.replace('automatic','tpiba').replace('scf','nscf')
-  nscf2 = nscf1.split('\n')
-  nscf2 = list(filter(None,nscf2))
+    # opens the template file from which it will do nscf calculations
+    scffile = directory + name_scf
+    with open(scffile, 'r') as templa:
+        original = templa.read()
+    templa.closed
+    nscf1 = original.replace('automatic', 'tpiba').replace('scf', 'nscf')
+    nscf2 = nscf1.split('\n')
+    nscf2 = list(filter(None, nscf2))
 
-  del nscf2[-1]                          # Delete SCF k points
+    del nscf2[-1]                          # Delete SCF k points
 
-  nscf = '\n'.join(nscf2)                # Build the template nscf file
+    nscf = '\n'.join(nscf2)                # Build the template nscf file
 
-#  sys.exit("Stop")
-  return nscf
+#    sys.exit("Stop")
+    return nscf
 
 
 
 
 # NSCF calculation ****************************************************
-def nscf(mpi,directory,name_nscf,nscf,nkps,kpoints,nbands):
+def nscf(mpi, directory, name_nscf, nscf, nkps, kpoints, nbands):
 
-  nscffile = directory + name_nscf
-  verifica = os.popen("test -f " + nscffile[:-3] + ".out;echo $?").read()   #if 1\n does not exist if 0\n exists
-  
-  if verifica == '1\n':  # If output file does not exist, run the nscf calculation
+    nscffile = directory + name_nscf
+   #if 1\n does not exist if 0\n exists
+    verifica = os.popen("test -f " + nscffile[:-3] + ".out;echo $?").read()
 
-    lines = nscf.split('\n')
-    subst = ''
-    flag = 0
-    for line in lines:
-      if line.find('SYSTEM') >= 0:
-        flag = 1
-      if line.find('nbnd') >= 0:
-        rep = line.split()
-        subst = subst + line.find('nbnd')*' '+'nbnd = ' + str(nbands) + ' ,\n'
-        flag = 2
-      elif flag == 1 and line.find('/') >= 0:
-        subst = subst + '                        '+'nbnd = ' + str(nbands) + ' ,\n'
-        subst = subst + line.rstrip() + '\n'
-        flag = 2
-      else:
-        subst = subst + line.rstrip() + '\n'
+    if verifica == '1\n':  # If output file does not exist, run the nscf calculation
 
-    nscf = subst
+        lines = nscf.split('\n')
+        subst = ''
+        flag = 0
+        for line in lines:
+            if line.find('SYSTEM') >= 0:
+                flag = 1
+            if line.find('nbnd') >= 0:
+                subst = subst + line.find('nbnd')*' ' + 'nbnd = ' + str(nbands) + ' ,\n'
+                flag = 2
+            elif flag == 1 and line.find('/') >= 0:
+                subst = subst + '                        '+'nbnd = ' + str(nbands) + ' ,\n'
+                subst = subst + line.rstrip() + '\n'
+                flag = 2
+            else:
+                subst = subst + line.rstrip() + '\n'
 
-    with open(nscffile,'w') as output:
-      output.write(nscf + str(nkps) + '\n' + kpoints)
-    output.closed
+        nscf = subst
 
-#    sys.exit("Stop")
+        with open(nscffile, 'w') as output:
+            output.write(nscf + str(nkps) + '\n' + kpoints)
+        output.closed
 
-    print('     Running nscf calculation')
-    comando = mpi + "pw.x -i " + nscffile + " > " + nscffile[:-3] + ".out"
-    print('     Command: '+comando+'\n')
-    os.system(comando)
-  return
+##       sys.exit("Stop")
 
-
+        print('     Running nscf calculation')
+        comando = mpi + "pw.x -i " + nscffile + " > " + nscffile[:-3] + ".out"
+        print('     Command: ' + comando+'\n')
+        os.system(comando)
 
 
 
 # wfck2r calculation and wavefunctions extraction *********************
-def wfck2r(nk1,nb1,total_bands=0):
+def wfck2r(nk1, nb1, total_bands=0):
 # These are the subroutines and functions
-  import loaddata as d 
+    import loaddata as d
 
-  if int(d.npr) == 1:
-    mpi = ''
-  else:
-    mpi = 'mpirun -np ' + str(d.npr) + ' '
+    if int(d.npr) == 1:
+        mpi = ''
+    else:
+        mpi = 'mpirun -np ' + str(d.npr) + ' '
 
-  comando = "&inputpp  prefix = '"+str(d.prefix)+"' ,\
-                       outdir = '"+str(d.outdir)+"' ,\
-                      first_k = "+str(nk1+1)+" ,\
-                       last_k = "+str(nk1+1)+" ,\
-                   first_band = "+str(nb1+1)+" ,\
-                    last_band = "+str(total_bands+1)+" , \
-                      loctave = .true., /"
-  sys.stdout.flush()
-  # Name of temporary file
-  filename = 'wfc'+str(nk1)+'-'+str(nb1)
-  # comand to send to the shell
-  cmd = 'echo "'+comando+'"|'+mpi+'wfck2r.x > tmp;tail -'+str(d.nr*((total_bands-nb1)+1))+' wfck2r.oct'
-  # Captures the output of the shell command
-  output = subprocess.check_output(cmd,shell=True)
-  # Strips the output of fortran formating and converts to numpy formating
-  out1 = output.decode('utf-8').replace(')','j')
-  out2 = out1.replace(', -','-')
-  out3 = out2.replace(',  ','+').replace('(','')
-  # puts the wavefunctions into a numpy array
-  psi  = np.fromstring(out3,dtype=complex,sep='\n')
+    comando = "&inputpp  prefix = '"+str(d.prefix)+"' ,\
+                         outdir = '"+str(d.outdir)+"' ,\
+                        first_k = "+str(nk1+1)+" ,\
+                         last_k = "+str(nk1+1)+" ,\
+                     first_band = "+str(nb1+1)+" ,\
+                      last_band = "+str(total_bands+1)+" , \
+                        loctave = .true., /"
+    sys.stdout.flush()
+    # Name of temporary file
+    filename = 'wfc' + str(nk1) + '-' + str(nb1)
+    # comand to send to the shell
+    cmd = 'echo "' + comando + '"|' + mpi + 'wfck2r.x > tmp;tail -' + str(d.nr*((total_bands-nb1)+1)) + ' wfck2r.oct'
+    # Captures the output of the shell command
+    output = subprocess.check_output(cmd, shell=True)
+    # Strips the output of fortran formating and converts to numpy formating
+    out1 = output.decode('utf-8').replace(')', 'j')
+    out2 = out1.replace(', -', '-')
+    out3 = out2.replace(',  ', '+').replace('(', '')
+    # puts the wavefunctions into a numpy array
+    psi = np.fromstring(out3, dtype=complex, sep='\n')
 
-  # For each band, find the value of teh wfc at the specific point rpoint (in real space)
-  psi_rpoint = np.array([psi[int(d.rpoint)+d.nr*i]for i in range((total_bands-nb1)+1)])
-  # Calculate the phase at rpoint for all the bands
-  deltaphase = np.arctan2(psi_rpoint.imag,psi_rpoint.real)
-  # and the modulus
-  mod_rpoint = np.absolute(psi_rpoint) 
-  psifinal = []
-  # For each band beeing considered
-  for i in range((total_bands-nb1)+1):
-    # Print to the output file for verification
-    print('    %6d  %4d  %12.8f  %12.8f   %r' % (nk1,i+nb1,mod_rpoint[i],deltaphase[i],not mod_rpoint[i] < 1E-5))
-    # Subtract the reference phase for each point
-    psifinal += list(psi[i*d.nr:(i+1)*d.nr]*np.exp(-1j*deltaphase[i]))
-  psifinal = np.array(psifinal)
+    # For each band, find the value of the wfc at the specific point rpoint (in real space)
+    psi_rpoint = np.array([psi[int(d.rpoint) + d.nr*i] for i in range((total_bands-nb1) + 1)])
+    # Calculate the phase at rpoint for all the bands
+    deltaphase = np.arctan2(psi_rpoint.imag, psi_rpoint.real)
+    # and the modulus
+    mod_rpoint = np.absolute(psi_rpoint)
+    psifinal = []
+    # For each band beeing considered
+    for i in range((total_bands-nb1) + 1):
+        # Print to the output file for verification
+        print('    %6d  %4d  %12.8f  %12.8f   %r' % (nk1, i+nb1, mod_rpoint[i], deltaphase[i], not mod_rpoint[i] < 1E-5))
+        # Subtract the reference phase for each point
+        psifinal += list(psi[i*d.nr:(i+1)*d.nr]*np.exp(-1j*deltaphase[i]))
+    psifinal = np.array(psifinal)
 
-  # Name of the final wfc file
-  outfiles = [str(d.wfcdirectory)+'k0'+str(nk1)+'b0'+str(band)+'.wfc' for band in range(nb1,total_bands+1)] 
-  # Save wavefunction to file
-  for i,outfile in enumerate(outfiles):
-    with open(outfile, 'wb') as f:
-      np.save(f,psifinal[i*d.nr:(i+1)*d.nr])
-    f.close()
-
-
-
+    # Name of the final wfc file
+    outfiles = [str(d.wfcdirectory) + 'k0' + str(nk1) + 'b0' + str(band) + '.wfc' for band in range(nb1, total_bands+1)]
+    # Save wavefunction to file
+    for i, outfile in enumerate(outfiles):
+        with open(outfile, 'wb') as fich:
+            np.save(fich, psifinal[i*d.nr:(i + 1)*d.nr])
+        fich.close()
--- python/generatewfc.py
+++ python/generatewfc.py
@@ -2,16 +2,12 @@
 # This program reads the wfc from DFT calculations make them coherent and saves
 #  in separate files
 ###################################################################################
-# 
+#
 # Can accept 0, 1 or 2 arguments.
 # If it has 0 arguments, it will run for all k-points and bands
 # If it has 1 argument, it will run just for one k-point, specified by the argument
 # If it has 2 arguments, it will run just for 1 k-point and 1 band, specified by the arguments
 
-# This is to include maths
-import numpy as np
-
-# This is to make operations in the shell
 import os
 import sys
 import time
@@ -19,69 +15,68 @@ import time
 # This are the subroutines and functions
 import contatempo
 import dft
-from headerfooter import header,footer
+from headerfooter import header, footer
 import loaddata as d
 
 ###################################################################################
 if __name__ == '__main__':
-  header('GENERATEWFC',time.asctime())
+    header('GENERATEWFC', time.asctime())
 
-  starttime = time.time()                         # Starts counting time
+    starttime = time.time()                         # Starts counting time
 
-  wfcdirectory = str(d.wfcdirectory)
-  dftdirectory = str(d.dftdirectory)
+    wfcdirectory = str(d.wfcdirectory)
+    dftdirectory = str(d.dftdirectory)
 
-  # Creates directory for wfc
-  os.system('mkdir -p '+wfcdirectory)
-  print('     Wavefunctions will be saved in directory',wfcdirectory)
-  print('     DFT files are in directory',dftdirectory)
-  npr = d.npr
-  print('     This program will run in '+str(npr)+' processors')
-  print()
-  nks = d.nks
-  print('     Total number of k-points:',nks)
-  nr1 = d.nr1
-  nr2 = d.nr2
-  nr3 = d.nr3
-  print('     Number of r-points in each direction:',nr1,nr2,nr3)
-  nr = d.nr
-  print('     Total number of points in real space:',nr)
-  nbnd = d.nbnd
-  print('     Number of bands:',nbnd)
-  print()
-  rpoint = int(d.rpoint)
-  print('     Point choosen for sincronizing phases: ',rpoint)
-  print()
+    # Creates directory for wfc
+    os.system('mkdir -p ' + wfcdirectory)
+    print('     Wavefunctions will be saved in directory', wfcdirectory)
+    print('     DFT files are in directory', dftdirectory)
+    npr = d.npr
+    print('     This program will run in ' + str(npr) + ' processors')
+    print()
+    nks = d.nks
+    print('     Total number of k-points:', nks)
+    nr1 = d.nr1
+    nr2 = d.nr2
+    nr3 = d.nr3
+    print('     Number of r-points in each direction:', nr1, nr2, nr3)
+    nr = d.nr
+    print('     Total number of points in real space:', nr)
+    nbnd = d.nbnd
+    print('     Number of bands:', nbnd)
+    print()
+    rpoint = int(d.rpoint)
+    print('     Point choosen for sincronizing phases: ', rpoint)
+    print()
 
   ##########################################################################
   # Creates files with wfc of bands at nk  ** DFT **
-  nk = -1
-  nb = -1
-  if len(sys.argv)==1:                      # Will run for all k-points and bands
-    print('     Will run for all k-points and bands')
-    print('     There are',nks,'k-points and',nbnd,'bands.')
-    for nk in range(nks):
-      print('     Calculating wfc for k-point',nk)
-      dft.wfck2r(nk,0,d.nbnd-1)
-  elif len(sys.argv)==2:                    # Will run just for k-point nk
-    nk = int(sys.argv[1])
-    print('     Will run just for k-point',nk)
-    print('     There are',nbnd,'bands.')
-    for nb in range(d.nbnd):
-      print('     Calculating wfc for k-point',nk,'and band',nb)
-      dft.wfck2r(nk,nb)
-  elif len(sys.argv)==3:                     # Will run just for k-point nk and band nb
-    nk = int(sys.argv[1])
-    nb = int(sys.argv[2])
-    print('     Will run just for k-point',nk,'and band',nb)
-    print('     Calculating wfc for k-point',nk,'and band',nb)
-    dft.wfck2r(nk,nb)
-  print()
+    nk = -1
+    nb = -1
+    if len(sys.argv) == 1:                    # Will run for all k-points and bands
+        print('     Will run for all k-points and bands')
+        print('     There are', nks, 'k-points and', nbnd, 'bands.')
+        for nk in range(nks):
+            print('     Calculating wfc for k-point', nk)
+            dft.wfck2r(nk, 0, d.nbnd-1)
+    elif len(sys.argv) == 2:                  # Will run just for k-point nk
+        nk = int(sys.argv[1])
+        print('     Will run just for k-point', nk)
+        print('     There are', nbnd, 'bands.')
+        for nb in range(d.nbnd):
+            print('     Calculating wfc for k-point', nk, 'and band', nb)
+            dft.wfck2r(nk, nb)
+    elif len(sys.argv) == 3:                  # Will run just for k-point nk and band nb
+        nk = int(sys.argv[1])
+        nb = int(sys.argv[2])
+        print('     Will run just for k-point', nk, 'and band', nb)
+        print('     Calculating wfc for k-point', nk, 'and band', nb)
+        dft.wfck2r(nk, nb)
+    print()
 
 
 ###################################################################################
-  # Finished
-  endtime = time.time()
-  
-  footer(contatempo.tempo(starttime,endtime))
-  
+    # Finished
+    endtime = time.time()
+
+    footer(contatempo.tempo(starttime, endtime))
