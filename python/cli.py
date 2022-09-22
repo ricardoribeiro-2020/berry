ef03383d64c739272ef27d2f479bc2b8346254e8
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-09-22 23:27:03 +0100

    Added condutivity_cli functionality

--- python/cli.py
+++ python/cli.py
@@ -72,19 +72,35 @@ def berry_props_cli() -> argparse.Namespace:
 def conductivity_cli() -> argparse.Namespace:
     # Define all cli arguments
     parser = argparse.ArgumentParser(description="")
-    
+    parser.add_argument("bandempty", type=int, help="Index of the conduction band.")
+    parser.add_argument("-enermax", type=float, default=2.5, help="Maximum energy in Ry units (default: 2.5).")
+    parser.add_argument("-enerstep", type=float, default=0.001, help="Energy step in Ry units (default: 0.001).")
+    parser.add_argument("-broadning", type=float, default=0.01, help="Energy broading in Ry units (default: 0.01).")
+    args = parser.parse_args()
+
     # Process the arguments given
+    args_dict = {}
+    args_dict["BANDEMPTY"] = args.bandempty
+    args_dict["ENERMAX"] = args.enermax
+    args_dict["ENERSTEP"] = args.enerstep
+    args_dict["BROADNING"] = args.broadning
 
     # Save them in a dict and return
-    
-    return
+    return args_dict
 
 def shg_cli() -> argparse.Namespace:
     # Define all cli arguments
     parser = argparse.ArgumentParser(description="")
-    
+    parser.add_argument("-enermax", type=float, default=2.5, help="Maximum energy in Ry units (default: 2.5).")
+    parser.add_argument("-enerstep", type=float, default=0.001, help="Energy step in Ry units (default: 0.001).")
+    parser.add_argument("-broadning", type=float, default=0.01, help="Energy broading in Ry units (default: 0.01).")
+    args = parser.parse_args()
+
     # Process the arguments given
+    args_dict = {}
+    args_dict["ENERMAX"] = args.enermax
+    args_dict["ENERSTEP"] = args.enerstep
+    args_dict["BROADNING"] = args.broadning
 
     # Save them in a dict and return
-    
-    return
+    return args_dict
