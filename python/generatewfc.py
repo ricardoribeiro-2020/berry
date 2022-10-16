2b7982a590c35a4ac72d6346a141452ba6a6cab3
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-10-16 10:44:57 +0100

    Added logging setup to generatewfc

--- python/generatewfc.py
+++ python/generatewfc.py
@@ -7,24 +7,22 @@
   If it has 2 arguments, it will run just for 1 k-point and 1 band, specified by the arguments
 """
 import os
-import sys
-import time
 
-from contatempo import tempo
 from cli import generatewfc_cli
-from headerfooter import header, footer
+from log_libs import log
 
 import dft
 import loaddata as d
+
+LOG = log("generatewfc", "GENERATE WFC", d.version)
 
 # pylint: disable=C0103
 ###################################################################################
 if __name__ == "__main__":
     args = generatewfc_cli()
 
-    header("GENERATEWFC", d.version, time.asctime())
+    LOG.header()
     os.system("mkdir -p " + d.wfcdirectory)
-    STARTTIME = time.time()
 
     ###########################################################################
     # 1. DEFINING THE CONSTANTS
@@ -35,41 +33,39 @@ if __name__ == "__main__":
     ###########################################################################
     # 2. STDOUT THE PARAMETERS
     ###########################################################################
-    print(f"\tUnique reference of run: {d.refname}", d.refname)
-    print(f"\tThis program will run in {d.npr} processors")
-    print(f"\tTotal number of k-points: {d.nks}")
-    print(f"\tNumber of bands: {d.nbnd}")
-    print(f"\tNumber of r-points in each direction: {d.nr1} {d.nr2} {d.nr3}")
-    print(f"\tPoint choosen for sincronizing phases: {d.rpoint}")
-    print(f"\tTotal number of points in real space: {d.nr}")
-    print(f"\tDFT files are in {d.dftdirectory}")
-    print(f"\tWavefunctions will be saved in {d.wfcdirectory}\n")
-    sys.stdout.flush()
+    LOG.info(f"\tUnique reference of run: {d.refname}", d.refname)
+    LOG.info(f"\tThis program will run in {d.npr} processors")
+    LOG.info(f"\tTotal number of k-points: {d.nks}")
+    LOG.info(f"\tNumber of bands: {d.nbnd}")
+    LOG.info(f"\tNumber of r-points in each direction: {d.nr1} {d.nr2} {d.nr3}")
+    LOG.info(f"\tPoint choosen for sincronizing phases: {d.rpoint}")
+    LOG.info(f"\tTotal number of points in real space: {d.nr}")
+    LOG.info(f"\tDFT files are in {d.dftdirectory}")
+    LOG.info(f"\tWavefunctions will be saved in {d.wfcdirectory}\n")
 
     ###########################################################################
     # 4. WAVEFUNCTIONS EXTRACTION
     ###########################################################################
     if isinstance(NK_POINTS, range):
-        print("\tWill run for all k-points and bands")
-        print(f"\tThere are {d.nks} k-points and {d.nbnd} bands.\n")
+        LOG.info("\tWill run for all k-points and bands")
+        LOG.info(f"\tThere are {d.nks} k-points and {d.nbnd} bands.\n")
 
         for nk in NK_POINTS:
-            print(f"\tCalculating wfc for k-point {nk}")
+            LOG.info(f"\tCalculating wfc for k-point {nk}")
             dft._wfck2r(nk, 0, d.nbnd)
     else:
         if isinstance(BANDS, range):
-            print(f"\tWill run just for k-point {NK_POINTS} an all bands.")
-            print(f"\tThere are {d.nks} k-points and {d.nbnd} bands.\n")
+            LOG.info(f"\tWill run just for k-point {NK_POINTS} an all bands.")
+            LOG.info(f"\tThere are {d.nks} k-points and {d.nbnd} bands.\n")
 
-            print(f"\tCalculating wfc for k-point {NK_POINTS}")
+            LOG.info(f"\tCalculating wfc for k-point {NK_POINTS}")
             dft._wfck2r(NK_POINTS, 0, d.nbnd)
         else:
-            print(f"\tWill run just for k-point {NK_POINTS} and band {BANDS}.\n")
+            LOG.info(f"\tWill run just for k-point {NK_POINTS} and band {BANDS}.\n")
             dft._wfck2r(NK_POINTS, BANDS)
-
 
     ###########################################################################
     # Finished
     ###########################################################################
 
-    footer(tempo(STARTTIME, time.time()))
+    LOG.footer()
