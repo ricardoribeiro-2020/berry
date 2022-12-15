f701c3059ab69dce95ee9840292178cea8081978
fabiocfabini <fabiolucas.carneiro@gmail.com>
2022-12-15 16:39:40 +0000

    Remove complex plot from geometry plot

--- berry/vis/_geometry.py
+++ berry/vis/_geometry.py
@@ -11,16 +11,13 @@ def bcc(band:int, grad:int, type: str):
     Q = np.hypot(np.imag(berry_conn[0]), np.imag(berry_conn[1]))  # Colors for imag part
 
     if type == "all":
-        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
         
         ax1.set_title("real space")
         ax1.quiver(np.real(berry_conn[0]), np.real(berry_conn[1]), M, units="x", width=0.042, scale=20 / 1,)
         
         ax2.set_title("imag space")
         ax2.quiver(np.imag(berry_conn[0]), np.imag(berry_conn[1]), Q, units="x", width=0.042, scale=20 / 1,)
-        
-        ax3.set_title("complex space")
-        ax3.quiver(berry_conn[0], berry_conn[1], color="b")
     else:
         fig, ax1 = plt.subplots()
         if type == "real":
@@ -29,9 +26,6 @@ def bcc(band:int, grad:int, type: str):
         elif type in "imag":
             ax1.set_title("imag")
             ax1.quiver(np.imag(berry_conn[0]), np.imag(berry_conn[1]), Q, units="x", width=0.042, scale=20 / 1,)
-        elif type in "complex":
-            ax1.set_title("complex")
-            ax1.quiver(berry_conn[0], berry_conn[1], color="b")
 
     # Add fig.suptitle() in bold
     fig.suptitle(f"Berry Connections {band}_{grad}\n", fontweight="bold")
