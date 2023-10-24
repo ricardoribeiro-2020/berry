cad9364c4987f82520653c7df0bb2ea5e09eda4a
Ricardo Mendes Ribeiro <ricardo@fisica.uminho.pt>
2023-10-24 16:53:59 +0100

    Added comments to preprocessing, variables for directory names and changes to datafile.npy

--- berry/_subroutines/loadmeta.py
+++ berry/_subroutines/loadmeta.py
@@ -5,20 +5,32 @@
 import numpy as np
 
 with open("datafile.npy", "rb") as fich:
+    version = str(np.load(fich))
+    refname = str(np.load(fich))
+
+    workdir = str(np.load(fich))
+    data_dir = str(np.load(fich))
+    log_dir = str(np.load(fich))
+    geometry_dir = str(np.load(fich))
+
     k0 = np.load(fich)
     nkx = int(np.load(fich))
     nky = int(np.load(fich))
     nkz = int(np.load(fich))
     nks = int(np.load(fich))
     step = float(np.load(fich))
-    npr = int(np.load(fich))
+    npr = int(np.load(fich))    
+    rpoint = int(np.load(fich))
+
     dftdirectory = str(np.load(fich))
     name_scf = str(np.load(fich))
     name_nscf = str(np.load(fich))
-    wfcdirectory = str(np.load(fich))
     prefix = str(np.load(fich))
+    wfcdirectory = str(np.load(fich))
     outdir = str(np.load(fich))
     dftdatafile = str(np.load(fich))
+    program = str(np.load(fich))
+
     a1 = np.load(fich)
     a2 = np.load(fich)
     a3 = np.load(fich)
@@ -30,15 +42,9 @@ with open("datafile.npy", "rb") as fich:
     nr3 = int(np.load(fich))
     nr = int(np.load(fich))
     nbnd = int(np.load(fich))
-    berrypath = str(np.load(fich))
-    rpoint = int(np.load(fich))
-    workdir = str(np.load(fich))
+  
     noncolin = str(np.load(fich))
-    program = str(np.load(fich))
     lsda = str(np.load(fich))
     nelec = float(np.load(fich))
-    prefix = str(np.load(fich))
     wfck2r = str(np.load(fich))
-    version = str(np.load(fich))
-    refname = str(np.load(fich))
     vb = int(np.load(fich))
