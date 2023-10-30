71d1b907c42be3247c74eedf4271e6901f92f9db
Ricardo Mendes Ribeiro <ricardo@fisica.uminho.pt>
2023-10-30 15:59:03 +0000

    changes in generatewfc.py

--- berry/generatewfc.py
+++ berry/generatewfc.py
@@ -9,8 +9,8 @@ import numpy as np
 from berry import log
 
 try:
-    import berry._subroutines.loaddata as d
     import berry._subroutines.loadmeta as m
+    import berry._subroutines.loaddata as d
 except:
     pass
 
@@ -44,19 +44,22 @@ class WfcGenerator:
     def run(self):
         # prints header on the log file
         self.logger.header()
-        # Sets the program used for converting wavefunctions to the real space
-        if m.noncolin:
-            self.k2r_program = "wfck2rFR.x"
-            self.logger.info("\n\tNoncolinear calculation, will use wfck2rFR.x")
-            self.logger.info("\t"+str(m.noncolin))
-        else:
-            self.k2r_program = "wfck2r.x"
-            self.logger.info("\n\tNonrelativistic calculation, will use wfck2r.x")
+
         # Logs the parameters for the run
         self._log_run_params()
 
+        # Sets the program used for converting wavefunctions to the real space
+        if m.noncolin:
+            self.k2r_program = "wfck2rFR.x"
+            self.logger.info("\tNoncolinear calculation, will use wfck2rFR.x")
+            self.logger.info("\t"+str(m.noncolin))
+        else:
+            self.k2r_program = "wfck2r.x"
+            self.logger.info("\tNonrelativistic calculation, will use wfck2r.x")
+
+        # Set which k-points and bands will use (for debuging)
         if isinstance(self.nk_points, range):
-            self.logger.info("\tWill run for all k-points and bands")
+            self.logger.info("\n\tWill run for all k-points and bands")
             self.logger.info(f"\tThere are {m.nks} k-points and {m.nbnd} bands.\n")
 
             for nk in self.nk_points:
@@ -95,41 +98,82 @@ class WfcGenerator:
         self.logger.info(f"\tPoint choosen for sincronizing phases:  {m.rpoint}\n")
 
     def _wfck2r(self, nk_point: int, initial_band: int, number_of_bands: int):
+        # Set the command to run
         shell_cmd = self._get_command(nk_point, initial_band, number_of_bands)
 
+        # Runs the command
         output = subprocess.check_output(shell_cmd, shell=True)
 
+        # Converts fortran complex numbers to numpy format
         out1 = (output.decode("utf-8")
                 .replace(")", "j")
                 .replace(", -", "-")
                 .replace(",  ", "+")
                 .replace("(", "")
                 )
+        
 
-        # puts the wavefunctions into a numpy array
-        psi = np.fromstring(out1, dtype=complex, sep="\n")
+        
 
-        # For each band, find the value of the wfc at the specific point rpoint (in real space)
-        psi_rpoint = np.array([psi[int(m.rpoint) + m.nr * i] for i in range(number_of_bands)])
+        if m.noncolin:
+            # puts the wavefunctions into a numpy array
+            psi = np.fromstring(out1, dtype=complex, sep="\n")
 
-        # Calculate the phase at rpoint for all the bands
-        deltaphase = np.arctan2(psi_rpoint.imag, psi_rpoint.real)
+            # For each band, find the value of the wfc at the specific point rpoint (in real space)
+            psi_rpoint = np.array([psi[int(m.rpoint) + m.nr * i] for i in range(0,2*number_of_bands,2)])
 
-        # and the modulus
-        mod_rpoint = np.absolute(psi_rpoint)
+            # Calculate the phase at rpoint for all the bands
+            deltaphase = np.arctan2(psi_rpoint.imag, psi_rpoint.real)
 
-        psifinal = []
-        for i in range(number_of_bands):
-            self.logger.debug(f"\t{nk_point:6d}  {(i + initial_band):4d}  {mod_rpoint[i]:12.8f}  {deltaphase[i]:12.8f}   {not mod_rpoint[i] < 1e-5}")
-            
-            # Subtract the reference phase for each point
-            psifinal += list(psi[i * m.nr : (i + 1) * m.nr] * np.exp(-1j * deltaphase[i]))
-        psifinal = np.array(psifinal)
+            # and the modulus of the wavefunction at the reference point rpoint (
+            # will be used to verify if the wavefunction at rpoint is significantly different from zero)
+            mod_rpoint = np.absolute(psi_rpoint)
 
-        outfiles = map(lambda band: os.path.join(m.wfcdirectory, f"k0{nk_point}b0{band+initial_band}.wfc"), range(number_of_bands))
-        for i, outfile in enumerate(outfiles):
-            with open(outfile, "wb") as fich:
-                np.save(fich, psifinal[i * m.nr : (i + 1) * m.nr])
+            psifinal0, psifinal1 = []
+
+            for i in range(0,2*number_of_bands,2):
+                self.logger.debug(f"\t{nk_point:6d}  {(i + initial_band):4d}  {mod_rpoint[i]:12.8f}  {deltaphase[i]:12.8f}   {not mod_rpoint[i] < 1e-5}")
+                
+                # Subtract the reference phase for each point
+                psifinal0 += list(psi[i * m.nr : (i + 1) * m.nr] * np.exp(-1j * deltaphase[i]))                # first part of spinor, all bands
+                psifinal1 += list(psi[m.nr + i * m.nr : m.nr + (i + 1) * m.nr] * np.exp(-1j * deltaphase[i]))  # second part of spinor, all bands
+
+            outfiles0 = map(lambda band: os.path.join(m.wfcdirectory, f"k0{nk_point}b0{band+initial_band}-0.wfc"), range(number_of_bands))
+            outfiles1 = map(lambda band: os.path.join(m.wfcdirectory, f"k0{nk_point}b0{band+initial_band}-1.wfc"), range(number_of_bands))
+
+            for i, outfile in enumerate(outfiles0):
+                with open(outfile, "wb") as fich:
+                    np.save(fich, psifinal0[i * m.nr : (i + 1) * m.nr])
+            for i, outfile in enumerate(outfiles1):
+                with open(outfile, "wb") as fich:
+                    np.save(fich, psifinal1[i * m.nr : (i + 1) * m.nr])
+
+        else:
+            # puts the wavefunctions into a numpy array
+            psi = np.fromstring(out1, dtype=complex, sep="\n")
+
+            # For each band, find the value of the wfc at the specific point rpoint (in real space)
+            psi_rpoint = np.array([psi[int(m.rpoint) + m.nr * i] for i in range(number_of_bands)])
+
+            # Calculate the phase at rpoint for all the bands
+            deltaphase = np.arctan2(psi_rpoint.imag, psi_rpoint.real)
+
+            # and the modulus of the wavefunction at the reference point rpoint (
+            # will be used to verify if the wavefunction at rpoint is significantly different from zero)
+            mod_rpoint = np.absolute(psi_rpoint)
+
+            psifinal = []
+            for i in range(number_of_bands):
+                self.logger.debug(f"\t{nk_point:6d}  {(i + initial_band):4d}  {mod_rpoint[i]:12.8f}  {deltaphase[i]:12.8f}   {not mod_rpoint[i] < 1e-5}")
+                
+                # Subtract the reference phase for each point
+                psifinal += list(psi[i * m.nr : (i + 1) * m.nr] * np.exp(-1j * deltaphase[i]))
+            psifinal = np.array(psifinal)
+
+            outfiles = map(lambda band: os.path.join(m.wfcdirectory, f"k0{nk_point}b0{band+initial_band}.wfc"), range(number_of_bands))
+            for i, outfile in enumerate(outfiles):
+                with open(outfile, "wb") as fich:
+                    np.save(fich, psifinal[i * m.nr : (i + 1) * m.nr])
 
     def _get_command(self, nk_point: int, initial_band: int, number_of_bands: int):
         mpi = "" if m.npr == 1 else f"mpirun -np {m.npr} "
