14c8b6a432f5f64e8eb1d4eaa3418500eb25ad91
Ricardo Mendes Ribeiro <ricardo@fisica.uminho.pt>
2024-01-22 16:50:05 +0000

    Added 3D to conductivity.py

--- berry/conductivity.py
+++ berry/conductivity.py
@@ -30,19 +30,41 @@ def load_berry_connections(conduction_band: int, berry_conn_size: int, berry_con
 
 def correct_eigenvalues(bandsfinal: np.ndarray) -> np.ndarray:
     kp = 0
-    eigen_array = np.zeros((m.nkx, m.nky, m.nbnd))
-
-    for j in range(m.nky):
+    if m.dimensions == 1:
+        eigen_array = np.zeros((m.nkx, m.nbnd))
         for i in range(m.nkx):
             for banda in range(m.nbnd):
-                eigen_array[i, j, banda] = d.eigenvalues[kp, bandsfinal[kp, banda]]
+                eigen_array[i, banda] = d.eigenvalues[kp, bandsfinal[kp, banda]]
             kp += 1
+    elif m.dimensions == 2:
+        eigen_array = np.zeros((m.nkx, m.nky, m.nbnd))
+        for j in range(m.nky):
+            for i in range(m.nkx):
+                for banda in range(m.nbnd):
+                    eigen_array[i, j, banda] = d.eigenvalues[kp, bandsfinal[kp, banda]]
+                kp += 1
+    else:
+        eigen_array = np.zeros((m.nkx, m.nky, m.nkz, m.nbnd))        
+        for l in range(m.nkz):
+            for j in range(m.nky):
+                for i in range(m.nkx):
+                    for banda in range(m.nbnd):
+                        eigen_array[i, j, l, banda] = d.eigenvalues[kp, bandsfinal[kp, banda]]
+                    kp += 1
+
 
     return eigen_array
 
 def get_delta_eigen_array_and_fermi(eigen_array: np.ndarray, conduction_band: int) -> Tuple[np.ndarray, np.ndarray]:
-    delta_eigen_array = np.zeros((m.nkx, m.nky, conduction_band + 1, conduction_band + 1))
-    fermi = np.zeros((m.nkx, m.nky, conduction_band + 1, conduction_band + 1))
+    if m.dimensions == 1:
+        delta_eigen_array = np.zeros((m.nkx, conduction_band + 1, conduction_band + 1))
+        fermi = np.zeros((m.nkx, conduction_band + 1, conduction_band + 1))
+    elif m.dimensions == 2:
+        delta_eigen_array = np.zeros((m.nkx, m.nky, conduction_band + 1, conduction_band + 1))
+        fermi = np.zeros((m.nkx, m.nky, conduction_band + 1, conduction_band + 1))
+    else:
+        delta_eigen_array = np.zeros((m.nkx, m.nky, m.nkz, conduction_band + 1, conduction_band + 1))
+        fermi = np.zeros((m.nkx, m.nky, m.nkz, conduction_band + 1, conduction_band + 1))
 
     for s in band_list:
         for sprime in band_list:
@@ -58,15 +80,15 @@ def get_delta_eigen_array_and_fermi(eigen_array: np.ndarray, conduction_band: in
 def compute_condutivity(omega:float, delta_eigen_array: np.ndarray, fermi: np.ndarray, broadning: complex) -> Tuple[float, np.ndarray]:
     omegaarray = np.full(OMEGA_SHAPE, omega + broadning)
     gamma = CONST * delta_eigen_array / (omegaarray - delta_eigen_array)        # factor that multiplies
-    sig = np.full((2, 2), 0.0 + 0j)                                             # matrix sig_xx, sig_xy, sig_yy, sig_yx
+    sig = np.full((m.dimensions, m.dimensions), 0.0 + 0j)                       # matrix sig_xx, sig_xy, sig_yy, sig_yx, etc
 
-    for s in band_list:                                                          # runs through index s
-        for sprime in band_list:                                                 # runs through index s'
+    for s in band_list:                                                         # runs through index s
+        for sprime in band_list:                                                # runs through index s'
             if s == sprime:
                 continue
 
-            for beta in range(2):                                               # beta is spatial coordinate
-                for alpha in range(2):                                          # alpha is spatial coordinate
+            for beta in range(m.dimensions):                                    # beta is spatial coordinate
+                for alpha in range(m.dimensions):                               # alpha is spatial coordinate
 
                     sig[alpha, beta] += np.sum(
                         gamma[:, :, sprime, s]
@@ -89,12 +111,12 @@ def run_conductivity(conduction_band: int, npr: int = 1, energy_max: float = 2.5
     # 1. DEFINING THE CONSTANTS
     ########################################################################### 
     RY    = 13.6056923                                                          # Conversion factor from Ry to eV
-    VK    = m.step * m.step / (2 * np.pi) ** 2                                  # element of volume in k-space in units of bohr^-1
+    VK    = (m.step / 2 * np.pi) ** m.dimensions                                # element of volume in k-space in units of bohr^-1
     # the '4' comes from spin degeneracy, that is summed in s and s'
     if m.noncolin:
-        CONST = 2j / (2 * np.pi) ** 2 
+        CONST = 2j / (2 * np.pi) ** m.dimensions
     else:
-        CONST = 4 * 2j / (2 * np.pi) ** 2                                           # = i2e^2/hslash 1/(2pi)^2     in Rydberg units
+        CONST = 4 * 2j / (2 * np.pi) ** m.dimensions                            # = i2e^2/hslash 1/(2pi)^2     in Rydberg units
 
     band_list   = list(range(conduction_band + 1))
 
@@ -103,9 +125,19 @@ def run_conductivity(conduction_band: int, npr: int = 1, energy_max: float = 2.5
     # Energy step (Ry)
     # energy broading (Ry)
 
-    OMEGA_SHAPE             = (m.nkx, m.nky, conduction_band + 1, conduction_band + 1)
-    berry_conn_size  = 2 * m.nkx * m.nky * (conduction_band + 1) ** 2
-    berry_conn_shape = (conduction_band + 1, conduction_band + 1, 2, m.nkx, m.nky)
+    if m.dimensions == 1:
+        OMEGA_SHAPE = (m.nkx, conduction_band + 1, conduction_band + 1)
+        berry_conn_size  = 2 * m.nkx * (conduction_band + 1) ** 2
+        berry_conn_shape = (conduction_band + 1, conduction_band + 1, 2, m.nkx)
+    elif m.dimensions == 2:
+        OMEGA_SHAPE = (m.nkx, m.nky, conduction_band + 1, conduction_band + 1)
+        berry_conn_size  = 2 * m.nkx * m.nky * (conduction_band + 1) ** 2
+        berry_conn_shape = (conduction_band + 1, conduction_band + 1, 2, m.nkx, m.nky)
+    else:
+        OMEGA_SHAPE = (m.nkx, m.nky, m.nkz, conduction_band + 1, conduction_band + 1)
+        berry_conn_size  = 2 * m.nkx * m.nky * m.nkz * (conduction_band + 1) ** 2
+        berry_conn_shape = (conduction_band + 1, conduction_band + 1, 2, m.nkx, m.nky, m.nkz)
+
     ###########################################################################
     # 2. STDOUT THE PARAMETERS
     ########################################################################### 
@@ -119,14 +151,19 @@ def run_conductivity(conduction_band: int, npr: int = 1, energy_max: float = 2.5
     logger.info(f"\n\tMaximum energy (Ry): {energy_max}")
     logger.info(f"\tEnergy step (Ry): {energy_step}")
     logger.info(f"\tEnergy broadning (Ry): {np.imag(broadning)}")
-    logger.info(f"\tConstant 4e^2/hslash 1/(2pi)^2 in Rydberg units: {np.imag(CONST)}")
+    if m.noncolin:
+        logger.info(f"\tThis is a noncolinear calculation.")
+        logger.info(f"\tConstant 2e^2/hslash 1/(2pi)^d in Rydberg units: {np.imag(CONST)}")
+    else:
+        logger.info(f"\tThis is a no spin calculation.")
+        logger.info(f"\tConstant 4e^2/hslash 1/(2pi)^d in Rydberg units: {np.imag(CONST)}")
+    logger.info(f"\tNumber of dimensions d = {m.dimensions}")
     logger.info(f"\tVolume (area) in k space: {VK}\n")
 
     ###########################################################################
     # 3. CREATE ALL THE ARRAYS
     ###########################################################################
     bandsfinal               = np.load(os.path.join(m.data_dir, "bandsfinal.npy"))
-    signalfinal              = np.load(os.path.join(m.data_dir, "signalfinal.npy"))                       #NOTE: Not used
     eigen_array              = correct_eigenvalues(bandsfinal)
     berry_connections        = load_berry_connections(conduction_band, berry_conn_size, berry_conn_shape)
     delta_eigen_array, fermi = get_delta_eigen_array_and_fermi(eigen_array, conduction_band)
@@ -142,33 +179,91 @@ def run_conductivity(conduction_band: int, npr: int = 1, energy_max: float = 2.5
     # 5. SAVE OUTPUT
     ###########################################################################
     with open(os.path.join(m.workdir, "sigmar.dat"), "w") as sigm:
-        sigm.write("# Energy (eV), sigma_xx,  sigma_yy,  sigma_yx,  sigma_xy\n")
-        for omega in np.arange(0, energy_max + energy_step, energy_step):
-            outp = "{0:.4f}  {1:.6f}  {2:.6f}  {3:.6f}  {4:.6f}\n"
-            sigm.write(
-                outp.format(
-                    omega * RY,
-                    np.real(sigma[omega][0, 0]),
-                    np.real(sigma[omega][1, 1]),
-                    np.real(sigma[omega][1, 0]),
-                    np.real(sigma[omega][0, 1]),
+        if m.dimensions == 1:
+            sigm.write("# Energy (eV), sigma\n")
+            for omega in np.arange(0, energy_max + energy_step, energy_step):
+                outp = "{0:.4f}  {1:.6f} \n"
+                sigm.write(
+                    outp.format(
+                        omega * RY,
+                        np.real(sigma[omega][0, 0])
+                    )
                 )
-            )
+        elif m.dimensions == 2:
+            sigm.write("# Energy (eV), sigma_xx,  sigma_yy,  sigma_yx,  sigma_xy\n")
+            for omega in np.arange(0, energy_max + energy_step, energy_step):
+                outp = "{0:.4f}  {1:.6f}  {2:.6f}  {3:.6f}  {4:.6f}\n"
+                sigm.write(
+                    outp.format(
+                        omega * RY,
+                        np.real(sigma[omega][0, 0]),
+                        np.real(sigma[omega][1, 1]),
+                        np.real(sigma[omega][1, 0]),
+                        np.real(sigma[omega][0, 1]),
+                    )
+                )
+        else:
+            sigm.write("# Energy (eV), sigma_xx,  sigma_yy,  sigma_zz,  sigma_xy, sigma_xz,  sigma_yx,  sigma_yz,  sigma_zx,  sigma_zy\n")
+            for omega in np.arange(0, energy_max + energy_step, energy_step):
+                outp = "{0:.4f}  {1:.6f}  {2:.6f}  {3:.6f}  {4:.6f}  {5:.6f}  {6:.6f}  {7:.6f}  {8:.6f}  {9:.6f}\n"
+                sigm.write(
+                    outp.format(
+                        omega * RY,
+                        np.real(sigma[omega][0, 0]),
+                        np.real(sigma[omega][1, 1]),
+                        np.real(sigma[omega][2, 2]),
+                        np.real(sigma[omega][0, 1]),
+                        np.real(sigma[omega][0, 2]),
+                        np.real(sigma[omega][1, 0]),
+                        np.real(sigma[omega][1, 2]),
+                        np.real(sigma[omega][2, 0]),
+                        np.real(sigma[omega][2, 1])
+                    )
+                )    
     logger.info("\tReal part of conductivity saved to file sigmar.dat")
 
     with open(os.path.join(m.workdir, "sigmai.dat"), "w") as sigm:
-        sigm.write("# Energy (eV), sigma_xx,  sigma_yy,  sigma_yx,  sigma_xy\n")
-        for omega in np.arange(0, energy_max + energy_step, energy_step):
-            outp = "{0:.4f}  {1:.6f}  {2:.6f}  {3:.6f}  {4:.6f}\n"
-            sigm.write(
-                outp.format(
-                    omega * RY,
-                    np.imag(sigma[omega][0, 0]),
-                    np.imag(sigma[omega][1, 1]),
-                    np.imag(sigma[omega][1, 0]),
-                    np.imag(sigma[omega][0, 1]),
+        if m.dimensions == 1:
+            sigm.write("# Energy (eV), sigma\n")
+            for omega in np.arange(0, energy_max + energy_step, energy_step):
+                outp = "{0:.4f}  {1:.6f} \n"
+                sigm.write(
+                    outp.format(
+                        omega * RY,
+                        np.imag(sigma[omega][0, 0])
+                    )
                 )
-            )
+        elif m.dimensions == 2:
+            sigm.write("# Energy (eV), sigma_xx,  sigma_yy,  sigma_yx,  sigma_xy\n")
+            for omega in np.arange(0, energy_max + energy_step, energy_step):
+                outp = "{0:.4f}  {1:.6f}  {2:.6f}  {3:.6f}  {4:.6f}\n"
+                sigm.write(
+                    outp.format(
+                        omega * RY,
+                        np.imag(sigma[omega][0, 0]),
+                        np.imag(sigma[omega][1, 1]),
+                        np.imag(sigma[omega][1, 0]),
+                        np.imag(sigma[omega][0, 1]),
+                    )
+                )
+        else:
+            sigm.write("# Energy (eV), sigma_xx,  sigma_yy,  sigma_zz,  sigma_xy, sigma_xz,  sigma_yx,  sigma_yz,  sigma_zx,  sigma_zy\n")
+            for omega in np.arange(0, energy_max + energy_step, energy_step):
+                outp = "{0:.4f}  {1:.6f}  {2:.6f}  {3:.6f}  {4:.6f}  {5:.6f}  {6:.6f}  {7:.6f}  {8:.6f}  {9:.6f}\n"
+                sigm.write(
+                    outp.format(
+                        omega * RY,
+                        np.imag(sigma[omega][0, 0]),
+                        np.imag(sigma[omega][1, 1]),
+                        np.imag(sigma[omega][2, 2]),
+                        np.imag(sigma[omega][0, 1]),
+                        np.imag(sigma[omega][0, 2]),
+                        np.imag(sigma[omega][1, 0]),
+                        np.imag(sigma[omega][1, 2]),
+                        np.imag(sigma[omega][2, 0]),
+                        np.imag(sigma[omega][2, 1])
+                    )
+                )    
     logger.info("\tImaginary part of conductivity saved to file sigmai.dat")
 
     ###########################################################################
