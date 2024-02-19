a600e91bb6f4662ea9bda2b0f6dc11a5161e3f1f
Daniel Sousa <fiscomp1984@gmail.com>
2024-02-19 23:18:45 +0000

    Added wfcut to dotproduct.py

--- berry/dotproduct.py
+++ berry/dotproduct.py
@@ -20,31 +20,41 @@ except:
 def dot(nk: int, j: int, neighbor: int, jNeighbor: Tuple[np.ndarray]) -> None:
     start = time()
 
+    initial_band = m.wfcut + 1
+
     dphase = d_phase[:, nk] * d_phase[:, neighbor].conj()
 
     if m.noncolin:  # Noncolinear case
-        for band0 in range(m.nbnd):
+        for band0 in range(initial_band,m.nbnd):
             wfc00 = np.load(os.path.join(m.wfcdirectory, f"k0{nk}b0{band0}-0.wfc"))
             wfc01 = np.load(os.path.join(m.wfcdirectory, f"k0{nk}b0{band0}-1.wfc"))
-            for band1 in range(m.nbnd):
+            for band1 in range(initial_band,m.nbnd):
                 wfc10 = np.load(os.path.join(m.wfcdirectory, f"k0{neighbor}b0{band1}-0.wfc")).conj()
                 wfc11 = np.load(os.path.join(m.wfcdirectory, f"k0{neighbor}b0{band1}-1.wfc")).conj()
                 
+                # Positions of the bands on the dpc array
+                b0 = band0 - initial_band
+                b1 = band1 - initial_band
+
                 # not normalized dot product
-                dpc[nk, j, band0, band1] = np.einsum("k,k,k->", dphase, wfc00, wfc10) + np.einsum("k,k,k->", dphase, wfc01, wfc11)
-                dpc[neighbor, jNeighbor, band1, band0] = dpc[nk, j, band0, band1].conj()
-                logger.debug(f"\t{nk}\t{band0}\t{j}\t{band1}\t",str(dpc[nk, j, band0, band1]))
+                dpc[nk, j, b0, b1] = np.einsum("k,k,k->", dphase, wfc00, wfc10) + np.einsum("k,k,k->", dphase, wfc01, wfc11)
+                dpc[neighbor, jNeighbor, b1, b0] = dpc[nk, j, b0, b1].conj()
+                logger.debug(f"\t{nk}\t{band0}\t{j}\t{band1}\t",str(dpc[nk, j, b0, b1]))
 
     else:  # Non-relativistic case
-        for band0 in range(m.nbnd):
+        for band0 in range(initial_band,m.nbnd):
             wfc0 = np.load(os.path.join(m.wfcdirectory, f"k0{nk}b0{band0}.wfc"))
-            for band1 in range(m.nbnd):
+            for band1 in range(initial_band,m.nbnd):
                 wfc1 = np.load(os.path.join(m.wfcdirectory, f"k0{neighbor}b0{band1}.wfc")).conj()
                 
+                # Positions of the bands on the dpc array
+                b0 = band0 - initial_band
+                b1 = band1 - initial_band
+
                 # not normalized dot product
-                dpc[nk, j, band0, band1] = np.einsum("k,k,k->", dphase, wfc0, wfc1)
-                dpc[neighbor, jNeighbor, band1, band0] = dpc[nk, j, band0, band1].conj()
-                logger.debug(f"\t{nk}\t{band0}\t{j}\t{band1}\t",str(dpc[nk, j, band0, band1]))
+                dpc[nk, j, b0, b1] = np.einsum("k,k,k->", dphase, wfc0, wfc1)
+                dpc[neighbor, jNeighbor, b1, b0] = dpc[nk, j, b0, b1].conj()
+                logger.debug(f"\t{nk}\t{band0}\t{j}\t{band1}\t",str(dpc[nk, j, b0, b1]))
 
     logger.debug(f"\tFinished of nk: {nk:>4}\tneighbor: {neighbor:>4}\tin: {(time() - start):>4.2f} seconds")
 
@@ -67,20 +77,25 @@ def run_dot(npr: int = 1, logger_name: str = "dot", logger_level: logging = logg
 
     logger.header()
 
+    number_of_bands = m.nbnd - m.wfcut - 1
+    initial_band = m.wfcut + 1
+    final_band = m.nbnd - 1
+
     ###########################################################################
     # 1. DEFINING THE CONSTANTS
     ########################################################################### 
-    DPC_SIZE = m.nks * 2 * m.dimensions * m.nbnd * m.nbnd
-    DPC_SHAPE = (m.nks, 2 * m.dimensions, m.nbnd, m.nbnd)
+    DPC_SIZE = m.nks * 2 * m.dimensions * number_of_bands * number_of_bands
+    DPC_SHAPE = (m.nks, 2 * m.dimensions, number_of_bands, number_of_bands)
 
     ###########################################################################
     # 2. STDOUT THE PARAMETERS
     ########################################################################### 
     logger.info(f"\tUnique reference of run: {m.refname}")
     logger.info(f"\tNumber of processors to use: {npr}")
-    logger.info(f"\tNumber of bands: {m.nbnd}")
     logger.info(f"\tTotal number of k-points: {m.nks}")
     logger.info(f"\tTotal number of points in real space: {m.nr}")
+    logger.info(f"\tWill use bands from {initial_band} to {final_band}")
+    logger.info(f"\tTotal number of bands to be used: {number_of_bands}\n")
     logger.info(f"\tDirectory where the wfc are: {m.wfcdirectory}\n")
 
     ###########################################################################
